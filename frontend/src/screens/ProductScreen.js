72732829f5c9eea5f1e3aae7dbc05bbba5de2afc
Rostyslav <rostuslav.nab@gmail.com>
2020-10-25 23:26:14 +0200

    Add Summary to ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -21,10 +21,11 @@ export const ProductScreen = ({match}) => {
                     <p>Description: {product.description}</p>
                 </Col>
                 <Col md={{span: 3}}>
-                    <h2>{product.name}</h2>
-                    <Rate disabled allowHalf defaultValue={product.rating}/> <span>{product.numReviews} reviews</span>
-                    <h3>Price: $ {product.price}</h3>
-                    <p>Description: {product.description}</p>
+                    <Card title="Summary" style={{ minWidth: 200 }}>
+                        <p>Price: <strong>$ {product.price}</strong></p>
+                        <p>Status: <strong>{product.countInStock > 1 ? 'In Stock' : 'Out of Stock'}</strong></p>
+                        <Button disabled={product.countInStock === 0}>Add to Cart</Button>
+                    </Card>
                 </Col>
             </Row>
         </>
